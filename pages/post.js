9f7b298c0134aea6d50e8660c6885c763c302c08
nwre87 <nwre87@gmail.com>
2017-07-20 00:29:05 -1000

    messin wit fetchin

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,23 @@
 import Layout from '../comps/MyLayout.js';
+import fetch from 'isomorphic-fetch';
 
-export default (props) => (
+const Post = (props) => (
   <Layout>
-    <h1>{props.url.query.title}</h1>
-    <p>This is the blog post content</p>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/[/]?p/g, '')}</p>
+    <img src={props.show.image.medium}/>
   </Layout>
 );
 
+
+Post.getInitialProps = async (context) => {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show}`);
+
+  return { show }
+}
+
+export default Post
