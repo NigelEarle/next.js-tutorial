0c4b941c3db10165fe88008c33436758bef3d08c
nwre87 <nwre87@gmail.com>
2017-07-19 17:07:17 -1000

    add PostLink comp

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Link from 'next/link';
 import Layout from '../comps/MyLayout.js';
+import Link from 'next/link';
 
-const Index = () => (
+const PostLink = (props) => (
+  <div>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </div>
+)
+
+export default () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
   </Layout>
 );
-
-export default Index
