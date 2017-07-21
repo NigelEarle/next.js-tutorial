72b34d306f681a6839b29e7a4631ce309a13c764
nwre87 <nwre87@gmail.com>
2017-07-20 15:28:20 -1000

    mess with global styles

--- pages/post.js
+++ pages/post.js
@@ -3,9 +3,23 @@ import fetch from 'isomorphic-fetch';
 
 const Post = (props) => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/[/]?p/g, '')}</p>
-    <img src={props.show.image.medium}/>
+    <h1 className="showName">{props.show.name}</h1>
+    <p className="showSummary">{props.show.summary.replace(/[/]?p/g, '')}</p>
+    <img className="showImage" src={props.show.image.medium}/>
+    <style jsx global>{`
+     .showName, .showSummary{
+       font-family: 'Arial';
+     }
+
+     .showSummary {
+       text-decoration: none;
+       color: blue;
+     }
+
+     .showImage{
+       height: auto;
+     }
+  `}</style>
   </Layout>
 );
 
